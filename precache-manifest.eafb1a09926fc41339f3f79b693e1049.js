9edc73811a7a5a60564f344c26f3215c14a1bcd1
mcast045 <54549449+mcast045@users.noreply.github.com>
2019-10-29 14:24:17 -0700

    Updates

---
+++ precache-manifest.eafb1a09926fc41339f3f79b693e1049.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d8e47eb900dac749db4d41f9aadf0f",
+    "url": "/weather-api/index.html"
+  },
+  {
+    "revision": "261e0bdce0e8d201c6f0",
+    "url": "/weather-api/static/css/main.cb59c118.chunk.css"
+  },
+  {
+    "revision": "5960f10b2aa7a7f123f1",
+    "url": "/weather-api/static/js/2.c7ad415b.chunk.js"
+  },
+  {
+    "revision": "261e0bdce0e8d201c6f0",
+    "url": "/weather-api/static/js/main.02f107eb.chunk.js"
+  },
+  {
+    "revision": "a104ea585fd9135dd611",
+    "url": "/weather-api/static/js/runtime~main.4827966c.js"
+  }
+]);
